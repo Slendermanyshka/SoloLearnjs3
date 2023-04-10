90c7f8c674bc07804e9ca01568912f6d3f66a31c
Slendermanyshka <slendermanyshka@gmail.com>
2023-04-10 22:07:23 +0300

    hw1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// import {SimpleLightbox} from '../../node_modules/simplelightbox/dist/simple-lightbox.js';
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+let imgMarkup = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>`;
+}).join('');
+
+galleryList.insertAdjacentHTML('beforeend', imgMarkup);
+
+    let instance = new SimpleLightbox('.gallery a', {
+        captionsData:'alt',
+        captionDelay: '250',
+    });
+    instance.on(() => {
+        instance.next();
+    });
