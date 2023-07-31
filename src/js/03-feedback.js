b5f74296e3a017b514edc09da76dd87f00720efe
Slendermanyshka <slendermanyshka@gmail.com>
2023-07-31 20:31:51 +0300

    upd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,18 @@
-var throttle = require('lodash.throttle');
+//var throttle = require('lodash.throttle');
 
 const forma = document.querySelector("feedback-form");
-// let values={
-//   email:"",
-//   message:""
-// };
+let values={
+  email:"",
+  message:""
+  };
 
-function getData(form){
-  var formData = new FormData(form);
-  console.log(formData);
 
+  const submit = function(e){
+    var formData = new FormData(document.querySelector('feedback-form'))
+    e.preventDefault();
+    console.log(formData);
+  }
+
+function saveData(data){
+  localStorage.setItem("feedback-form-state", data)
 }
